d52f9989ba993fae064358254a4b5acd0060e419
Evgenii Shumilov <mechnotech@yandex.ru>
2020-04-20 17:11:36 +0300

    v 0.1

--- main.py
+++ main.py
@@ -27,18 +27,14 @@ toasted_bread 5 cnt 7.3 1.6 52.3 248
 butter 100 g 0.8 72.5 1.3 661'''
 
 INPUT = INPUT.split('\n')
-Num_Dish = int(INPUT[0])  # Число блюд
-
 pos = []
 for i in INPUT:
     if 1 <= len(i) < 4:
         pos.append(INPUT.index(i))
 
 Recipt_List = INPUT[1:pos[1]]
-Price_List = INPUT[pos[1]+1:pos[2]]
-Calories_List = INPUT[pos[2]+1:]
-
-#  Dish_List = ['omlet', 9, ['butter 10 g', 'egg 5 cnt', 'etc']]
+Price_List = INPUT[pos[1] + 1:pos[2]]
+Calories_List = INPUT[pos[2] + 1:]
 
 Dish_List = []
 for i in Recipt_List:
@@ -46,15 +42,10 @@ for i in Recipt_List:
     try:
         e = int(pos[2])
         s = Recipt_List.index(i)
-        dish = pos[0], pos[1], Recipt_List[s+1: s+e+1]
+        dish = pos[0], pos[1], Recipt_List[s + 1: s + e + 1]
         Dish_List.append(dish)
     except ValueError:
         pass
-
-print(Dish_List)
-# print(Recipt_List)
-print(Price_List)
-# print(Calories_List)
 
 
 class CatalogNutr:
@@ -64,16 +55,21 @@ class CatalogNutr:
     def add_record(self, nutr):
         self.records.append(nutr)
 
-    def show_plc(self, nutr, quant): # вернуть расчет БЖУ в пересчете на число единиц (граммов, мл, шт)
+    def show_plc(self, nutr, quant):  # вернуть расчет БЖУ в пересчете на число единиц (граммов, мл, шт)
         for i in self.records:
             if nutr in i.name:
-                return i.proteins*quant, i.lipids*quant, i.carbs*quant, i.callories*quant
+                return i.proteins * quant, i.lipids * quant, i.carbs * quant, i.callories * quant
 
-    def show_price(self, nutr):
+
+class PriceCatalog:
+    def __init__(self):
+        self.records = []
+
+    def add_record(self, prod):
+        self.records.append(prod)
+
+    def show_price(self, prod):
         pass
-
-
-
 
 
 class Nutrient:
@@ -94,37 +90,62 @@ class Nutrient:
         if self.name_quant == 'tens':
             delimiter = 10
             self.name_quant = 'cnt'
-        self.proteins = round((float(string[3])/delimiter), 5)
-        self.lipids = round((float(string[4])/delimiter), 5)
-        self.carbs = round((float(string[5])/delimiter), 5)
-        self.callories = round((float(string[6])/delimiter), 5)
+        self.proteins = round((float(string[3]) / delimiter), 5)
+        self.lipids = round((float(string[4]) / delimiter), 5)
+        self.carbs = round((float(string[5]) / delimiter), 5)
+        self.callories = round((float(string[6]) / delimiter), 5)
 
-class Dish:
-    def __init__(self):
-        components = {}
+
+def Show_Quantity(product_dict):
+    sum_price = 0
+    for k, v in product_dict.items():
+        for j in Price_List:
+            row = j.split(' ')
+            if k in row[0]:
+                price = int(row[1])
+                quant = int(row[2])
+                name_quant = row[3]
+                if name_quant == 'kg' or name_quant == 'l':
+                    quant = quant * 1000
+                if name_quant == 'tens':
+                    quant = quant * 10
+                ost = int(v / quant) + 1
+                sum_price += price * ost
+                product_dict[k] = ost
+    return product_dict, sum_price
+
 
 Calor_Obj = CatalogNutr()
 for i in Calories_List:  # Заполнить справочник БЖУ и К
     Calor_Obj.add_record(nutr=Nutrient(string=i))
 
-# Вывод блюд и их БЖК и К
+# Вывод блюд и их БЖК и К, создаем словарь с числом единиц каждого продукта
+product_dict = {}
+callories_otput = []
 for i in Dish_List:
     list_ingr = i[2]
+    num_person = int(i[1])
     p = l = c = cl = 0
+
     for j in list_ingr:
         ingr = j.split(' ')
         plc = Calor_Obj.show_plc(nutr=ingr[0], quant=int(ingr[1]))
+        if ingr[0] in product_dict.keys():
+
+            old_v = product_dict[ingr[0]]
+            key = ingr[0]
+            product_dict.update({key: old_v + num_person * int(ingr[1])})
+        else:
+            product_dict[ingr[0]] = num_person * int(ingr[1])
         p += plc[0]
         l += plc[1]
         c += plc[2]
         cl += plc[3]
-    print(i[0], p, l, c, cl)
+    callories_otput.append((i[0], p, l, c, cl))
 
-# print('''sandwich 6.00 13.29 21.50 228.3
-# omelet 57.360 57.540 5.314 177.800''')
-#Calor_Obj.show_plc(nutr='chocolate', quant=90)
-#Calor_Obj.show_all()
-
-
-
-
+new_dict, sum_prce = Show_Quantity(product_dict)
+print(sum_prce)
+for k, v in product_dict.items():
+    print(k, v)
+for i in callories_otput:
+    print(' '.join(map(str, i)))
